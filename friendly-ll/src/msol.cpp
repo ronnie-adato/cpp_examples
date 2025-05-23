7ef3f3827708044b8131faf940111ad2391de2d2
Ronnie Adato <ronnie.adato@gmail.com>
2025-05-23 19:30:18 +0300

    test is failing after change

--- friendly-ll/src/msol.cpp
+++ friendly-ll/src/msol.cpp
@@ -8,31 +8,37 @@ auto MSol::Clone(Node *root) -> Node * {
     LOG(ERROR) << "Error: root is nullptr";
     return nullptr;
   }
-  LOG(INFO) << "Begin: " << root;
 
-  Node *cr = nullptr;
-  for (Node *r = root; r != nullptr; r->next = cr, r = cr->next) {
-    cr = new Node();
+  LOG(INFO) << "Begin: " << root;
+  
+  auto r = root;
+  while (r != nullptr) {
+    
+    auto cr = new Node();
     cr->value = r->value + 10;
     cr->next = r->next;
+    r->next = cr;
+    r = r->next->next;
   }
   LOG(INFO) << "Phase 1: " << root;
 
-  for (Node *r = root; r != nullptr; r = r->next->next) {
+  r = root;
+  while (r != nullptr) {
     r->next->afriend = r->afriend->next;
+    r = r->next->next;
   }
   LOG(INFO) << "Phase 2: " << root;
 
-  Node *croot = root->next;
+  auto croot = root->next;
+  auto ctmp = croot;
+  r = root;
 
-  for (Node *r = root, *ctmp = croot; r != nullptr;
-       r = r->next, ctmp = ctmp->next) {
+  while (ctmp->next != nullptr) {
     r->next = ctmp->next;
-    if (ctmp->next != nullptr) {
-      ctmp->next = ctmp->next->next;
-    }
+    ctmp->next = ctmp->next->next;
+    r = r->next;
+    ctmp = ctmp->next;
   }
-
   LOG(INFO) << "root: " << root;
   LOG(INFO) << "croot: " << croot;
   return croot;
