17e9013c45f9418d2f0dcbdd907fb312832bb49a
Ronnie Adato <ronnie.adato@gmail.com>
2025-05-28 13:20:15 +0300

    Unified tests under "typed test suit"

---
+++ friendly-ll/tests/include/clone_solution_typed_test.hpp
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <memory>
+
+class Solution;
+
+template <typename T> class CloneSolutionTypedTest : public ::testing::Test {
+    public:
+    std::unique_ptr<Solution> solution = std::make_unique<T>();
+};
