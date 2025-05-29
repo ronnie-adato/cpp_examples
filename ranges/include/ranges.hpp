1fb72279db00685dfbabacdebed28f52576428ea
Ronnie Adato <ronnie.adato@gmail.com>
2025-05-29 11:01:33 +0300

    Added logging to base class Ranges

--- ranges/include/ranges.hpp
+++ ranges/include/ranges.hpp
@@ -1,8 +1,13 @@
 #pragma once
 
+#include <ostream>
+
 class Ranges {
+  friend std::ostream &operator<<(std::ostream &stream, const Ranges &ranges);
+
 public:
-  virtual void add_range(int left, int right) = 0;
-  virtual void remove_range(int left, int right) = 0;
-  virtual bool query_range(int left, int right) = 0;
+  virtual void add_range(int left, int right);
+  virtual void remove_range(int left, int right);
+  virtual bool query_range(int left, int right);
 };
+
