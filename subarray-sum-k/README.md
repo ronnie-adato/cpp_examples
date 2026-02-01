b79e75beecef8dcfe10a2d7f2a028934cc6dd598
Ronnie Adato <ronnie.adato@gmail.com>
2026-02-01 16:36:22 +0200

    chore(subarray-sum-k): scaffold module

---
+++ subarray-sum-k/README.md
@@ -0,0 +1,7 @@
+# Subarray Sum Equals K (LeetCode 560, Medium)
+
+LeetCode: <https://leetcode.com/problems/subarray-sum-equals-k/>
+
+Scaffold only: Bazel package + C++ header/source + gtest skeleton.
+
+**Topics**: Prefix Sum, Hash Map
---
+++ subarray-sum-k/src/subarray_sum_k.cpp
@@ -0,0 +1,11 @@
+#include "subarray_sum_k.hpp"
+
+#include <ng-log/logging.h>
+
+int SubarraySumK::subarraySum(const std::vector<int>& nums, int k)
+{
+    (void)nums;
+    (void)k;
+    LOG(INFO) << "SubarraySumK::subarraySum called (scaffold)";
+    return 0;
+}
