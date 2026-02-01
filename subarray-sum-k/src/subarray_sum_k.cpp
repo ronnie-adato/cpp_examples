1d46f2c0af92112fc3905396421f413d3bf11f90
Ronnie Adato <ronnie.adato@gmail.com>
2026-02-01 16:36:31 +0200

    test(subarray-sum-k): add comprehensive gtest suite

--- subarray-sum-k/src/subarray_sum_k.cpp
+++ subarray-sum-k/src/subarray_sum_k.cpp
@@ -2,10 +2,24 @@
 
 #include <ng-log/logging.h>
 
+#include <cstddef>
+
 int SubarraySumK::subarraySum(const std::vector<int>& nums, int k)
 {
-    (void)nums;
-    (void)k;
-    LOG(INFO) << "SubarraySumK::subarraySum called (scaffold)";
-    return 0;
+    LOG(INFO) << "SubarraySumK::subarraySum called (naive)";
+
+    long long count = 0;
+    for (size_t i = 0; i < nums.size(); ++i)
+    {
+        long long sum = 0;
+        for (size_t j = i; j < nums.size(); ++j)
+        {
+            sum += nums[j];
+            if (sum == static_cast<long long>(k))
+            {
+                ++count;
+            }
+        }
+    }
+    return static_cast<int>(count);
 }
