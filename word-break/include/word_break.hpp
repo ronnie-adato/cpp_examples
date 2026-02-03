0e4bdd32984fd18f51823845474dba952795104f
Ronnie Adato <ronnie.adato@gmail.com>
2026-02-03 15:36:22 +0200

    refactor(word-break): simplify prefix recursion

--- word-break/include/word_break.hpp
+++ word-break/include/word_break.hpp
@@ -1,20 +1,13 @@
 #pragma once
 
-#include <memory>
 #include <set>
 #include <string>
 #include <vector>
 
 class WordBreak
 {
-    struct TreeNode
-    {
-        std::string word;
-        std::vector<std::shared_ptr<TreeNode>> children;
-    };
-
-    bool wordBreak(TreeNode *node, const std::string &s, const std::set<std::string> &wordDict);
+    bool wordBreak(const std::string& s, const std::set<std::string_view>& wordDict);
 
    public:
-    bool wordBreak(const std::string &s, const std::vector<std::string> &wordDict);
+    bool wordBreak(const std::string& s, const std::vector<std::string>& wordDict);
 };
